57c4b8ab56a50d343cd66786fa219ef073dc4875
Igor Nemykin <i.nemykin@devcaz.com>
2022-09-17 15:50:38 +0200

    add icon to Result component, change view

--- src/components/Search/Result.vue
+++ src/components/Search/Result.vue
@@ -1,17 +1,24 @@
 <script setup>
+  import { computed } from 'vue';
+  import { Select } from '@element-plus/icons-vue';
   import { useBookingStore } from '@/stores/booking';
-  
+
   // eslint-disable-next-line no-unused-vars
   const props = defineProps({
     booking: Object,
   });
+
   const store = useBookingStore();
-  const { setBooking } = store;
+  const { booking: selectedBooking, setBooking } = store;
+  const showIcon = computed(() => props.booking.orderId === selectedBooking.orderId ? 'Edit' : null);
 </script>
 
 <template>
-    <div>
-      <el-link :underline="false" @click="setBooking(booking)">{{ booking.fullName }}, Booring Order № {{ booking.orderId }}</el-link>
-    </div>
+  <el-link :underline="false" @click="setBooking(booking)">
+    <el-icon class="el-icon--left" >
+      <Select v-if="showIcon" />
+    </el-icon>
+    {{ booking.fullName }}, Booring Order № {{ booking.orderId }}
+  </el-link>
   <el-divider />
 </template>
