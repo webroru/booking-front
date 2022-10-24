1a2f876ee24ddede29d1ee62c4707b09702e9f01
Igor Nemykin <i.nemykin@devcaz.com>
2022-10-24 10:17:39 +0200

    return back an extended booking info to the search page

--- src/components/Search/Result.vue
+++ src/components/Search/Result.vue
@@ -14,11 +14,26 @@
 </script>
 
 <template>
-  <el-link :underline="false" @click="setBooking(booking)">
-    <el-icon class="el-icon--left" >
-      <Select v-if="showIcon" />
-    </el-icon>
-    {{ booking.fullName }}, Booring Order № {{ booking.orderId }}
-  </el-link>
-  <el-divider />
+    <el-descriptions :title="booking.fullName" @click="setBooking(booking)" border :column="3" class="item">
+      <el-descriptions-item>
+        <template #label>
+          <el-icon class="el-icon--left">
+            <Select v-if="showIcon" />
+          </el-icon>
+          Property
+        </template>
+        {{ booking.propertyName }}
+      </el-descriptions-item>
+      <el-descriptions-item label="Room">{{ booking.room }}</el-descriptions-item>
+      <el-descriptions-item label="Check-in">{{ booking.checkInDate }}</el-descriptions-item>
+      <el-descriptions-item label="Check-out">{{ booking.checkOutDate }}</el-descriptions-item>
+      <el-descriptions-item label="Original Referrer">{{ booking.originalReferrer }}</el-descriptions-item>
+    </el-descriptions>
 </template>
+
+<style scoped>
+  .item {
+    cursor: pointer;
+    margin: 0 0 20px;
+  }
+</style>
