2e69e3a16b51eedc54bdb6d552aab8e1b5eca1a4
Igor Nemykin <webroru@gmail.com>
2024-10-05 16:42:44 +0200

    fix structure refactoring

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,60 @@
+<script setup>
+  import { ref, onMounted, watch, computed } from 'vue';
+  import { useRouter, useRoute } from 'vue-router';
+  import { useBookingStore } from '@/stores/booking';
+  import { useInfoStore } from '@/stores/info';
+  import { usePhotosStore } from '@/stores/photos';
+  import config from '@/config';
+
+  const bookingStore = useBookingStore();
+  const {bookings, resetBooking, searchBooking, setBookings} = bookingStore;
+  const infoStore = useInfoStore();
+  const { getInfo } = infoStore;
+  const photosStore = usePhotosStore();
+  const { clearPhotosStore, syncPhotos } = photosStore;
+  const date = ref('');
+  const inActiveTime = 60000;
+  const lastActivity = new Date();
+  const router = useRouter();
+  const route = useRoute();
+  const orderId = computed(() => Number(route.params.orderId));
+  const path = computed(() => route.path);
+
+  onMounted(() => {
+    setInterval(() => date.value = new Date().toLocaleTimeString(), 100);
+    setInterval(resetInactivePage, inActiveTime);
+    getInfo('en');
+  });
+
+  watch(
+    [orderId, path],
+    ([orderId, path]) => {
+      fetchBookingByUrlParam(orderId);
+      if (path === '/') {
+        resetBooking();
+        clearPhotosStore();
+      }
+    }
+  );
+
+  const resetInactivePage = () => {
+    const currentTime = new Date();
+    if (currentTime.getTime() - lastActivity.getTime() >= inActiveTime && config.env === 'prod') {
+      router.push('/');
+    }
+  };
+
+  const fetchBookingByUrlParam = async (orderId) => {
+    if (orderId && !bookings.some(booking => booking.orderId === orderId)) {
+      const {data} = await searchBooking(orderId);
+      if (data.length) {
+        setBookings(data);
+        syncPhotos();
+      }
+    }
+  };
+</script>
+
 <template>
   <div id="app">
     <router-view />
--- src/layouts/DefaultLayout.vue
+++ src/layouts/DefaultLayout.vue
@@ -1,43 +1,26 @@
 <script setup>
-  import {ref, onMounted, watch, computed} from 'vue';
-  import {useRouter, useRoute} from 'vue-router';
-  import {useBookingStore} from '@/stores/booking';
-  import {useInfoStore} from '@/stores/info';
-  import {usePhotosStore} from '@/stores/photos';
+  import { ref, onMounted } from 'vue';
+  import { useRouter } from 'vue-router';
+  import { useInfoStore } from '@/stores/info';
+  import { useBookingStore } from '@/stores/booking';
   import config from '@/config';
   import LanguageSelect from '@/components/LanguageSelect.vue';
   import HotelAddress from '@/components/HotelAddress.vue';
 
   const bookingStore = useBookingStore();
-  const {bookings, resetBooking, searchBooking, setBookings} = bookingStore;
+  const { bookings } = bookingStore;
   const infoStore = useInfoStore();
-  const {getInfo} = infoStore;
-  const photosStore = usePhotosStore();
-  const {clearPhotosStore, syncPhotos} = photosStore;
+  const { getInfo } = infoStore;
   const date = ref('');
   const inActiveTime = 60000;
   const lastActivity = new Date();
   const router = useRouter();
-  const route = useRoute();
-  const orderId = computed(() => Number(route.params.orderId));
-  const path = computed(() => route.path);
 
   onMounted(() => {
     setInterval(() => date.value = new Date().toLocaleTimeString(), 100);
     setInterval(resetInactivePage, inActiveTime);
     getInfo('en');
   });
-
-  watch(
-    [orderId, path],
-    ([orderId, path]) => {
-      fetchBookingByUrlParam(orderId);
-      if (path === '/') {
-        resetBooking();
-        clearPhotosStore();
-      }
-    }
-  );
 
   document.addEventListener('click', () => {
     lastActivity.setTime(new Date());
@@ -47,16 +30,6 @@
     const currentTime = new Date();
     if (currentTime.getTime() - lastActivity.getTime() >= inActiveTime && config.env === 'prod') {
       router.push('/');
-    }
-  };
-
-  const fetchBookingByUrlParam = async (orderId) => {
-    if (orderId && !bookings.some(booking => booking.orderId === orderId)) {
-      const {data} = await searchBooking(orderId);
-      if (data.length) {
-        setBookings(data);
-        syncPhotos();
-      }
     }
   };
 </script>
