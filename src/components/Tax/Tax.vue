154da26970d644192625c4466ae66141b406bfaa
Igor Nemykin <igor.nemykin@dhl.com>
2025-04-05 11:57:17 +0200

    [GREEN-57] regula component integration

--- src/components/Tax/Tax.vue
+++ src/components/Tax/Tax.vue
@@ -1,31 +1,11 @@
 <script setup>
-  import { ref } from 'vue';
   import { useBookingStore } from '@/stores/booking';
-  import MakePhoto from '@/components/Photos/MakePhoto.vue';
   import TaxItem from '@/components/Tax/TaxItem.vue';
 
   const store = useBookingStore();
   const { bookings } = store;
-  const showMakePhoto = ref(false);
-  const isCameraEnabled = ref(false);
-  const makePhotoOrderId = ref();
-
-  const openMakePhoto = (orderId) => {
-    isCameraEnabled.value = true;
-    showMakePhoto.value = true;
-    makePhotoOrderId.value = orderId;
-  };
-
-  const closeMakePhoto = () => {
-    isCameraEnabled.value = false;
-    showMakePhoto.value = false;
-  };
 </script>
 
 <template>
-  <TaxItem v-for="booking in bookings" :key="booking.orderId" :booking="booking" @open-make-photo="openMakePhoto"/>
-  <el-dialog v-model="showMakePhoto" :title="$t('photos.makePhotoTitle')" width="80%"
-    :before-close="closeMakePhoto">
-    <make-photo :is-camera-enabled="isCameraEnabled" :order-id="makePhotoOrderId" />
-  </el-dialog>
+  <TaxItem v-for="booking in bookings" :key="booking.orderId" :booking="booking" />
 </template>
