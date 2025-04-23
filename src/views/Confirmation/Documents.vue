167d9a6a1022d55f2045f2e9d8cc9784382ea6bd
Igor Nemykin <igor.nemykin@dhl.com>
2025-04-23 17:29:15 +0200

    [GREEN-56] add Guide component

--- src/views/Confirmation/Documents.vue
+++ src/views/Confirmation/Documents.vue
@@ -1,11 +1,13 @@
 <script setup>
   import { useBookingStore } from '@/stores/booking';
   import Documents from '@/components/Documents/Documents.vue';
+  import Guide from '@/components/Documents/Guide.vue';
 
   const store = useBookingStore();
   const { bookings } = store;
 </script>
 
 <template>
+  <guide />
   <documents v-for="booking in bookings" :key="booking.orderId" :booking="booking" />
 </template>
