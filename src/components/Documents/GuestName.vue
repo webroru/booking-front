fd9ad6d220642ff580623adb2fc42c6c81e2658e
Igor Nemykin <igor.nemykin@dhl.com>
2025-05-02 16:41:39 +0200

    [GREEN-56] show short guest information

---
+++ src/components/Documents/GuestName.vue
@@ -0,0 +1,15 @@
+<script setup>
+  import { Delete } from '@element-plus/icons-vue';
+
+  defineProps({
+    guest: Object,
+    index: Number,
+  });
+</script>
+
+<template>
+  <el-row align="middle">
+    <el-col :span="18">{{guest.firstName }} {{guest.lastName}}</el-col>
+    <el-col :span="6"><el-button type="danger" :icon="Delete" circle @click="$emit('remove', index)" /></el-col>
+  </el-row>
+</template>
