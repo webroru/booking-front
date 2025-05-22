212c0b1e0708e09c825dfb395cdc3cc319b8f77b
webroru <webroru@gmail.com>
2025-05-22 19:19:45 +0200

    [GREEN-63] manual guest data (#25)

---
+++ src/components/Documents/GuestForm.vue
@@ -0,0 +1,48 @@
+<script setup>
+  import { reactive } from 'vue';
+  import { useI18n } from 'vue-i18n';
+
+  const { t } = useI18n();
+  const props = defineProps({ guest: Object });
+  const localGuest = reactive({ ...props.guest });
+</script>
+
+<template>
+  <el-form :model="localGuest" label-width="auto" style="max-width: 600px">
+    <el-form-item v-if="!guest.firstName" :label="t('guest.firstName')">
+      <el-input v-model="localGuest.firstName" />
+    </el-form-item>
+    <el-form-item v-if="!guest.lastName" :label="t('guest.lastName')">
+      <el-input v-model="localGuest.lastName" />
+    </el-form-item>
+    <el-form-item v-if="!guest.dateOfBirth" :label="t('guest.dateOfBirth')">
+        <el-date-picker
+            v-model="localGuest.dateOfBirth"
+            type="date"
+            :placeholder="t('guest.pickADate')"
+            style="width: 100%"
+        />
+    </el-form-item>
+    <el-form-item v-if="!guest.gender" :label="t('guest.gender')">
+      <el-radio-group v-model="localGuest.gender">
+        <el-radio label="M">{{ $t('guest.male') }}</el-radio>
+        <el-radio label="F">{{ $t('guest.female') }}</el-radio>
+      </el-radio-group>
+    </el-form-item>
+    <el-form-item v-if="!guest.nationality" :label="t('guest.nationality')">
+      <el-input v-model="localGuest.nationality" />
+    </el-form-item>
+    <el-form-item v-if="!guest.documentType" :label="t('guest.documentType')">
+      <el-radio-group v-model="localGuest.documentType">
+        <el-radio label="passport">{{ $t('guest.passport') }}</el-radio>
+        <el-radio label="ID">{{ $t('guest.id') }}</el-radio>
+      </el-radio-group>
+    </el-form-item>
+    <el-form-item v-if="!guest.documentNumber" :label="t('guest.documentNumber')">
+      <el-input v-model="localGuest.documentNumber" />
+    </el-form-item>
+    <el-form-item>
+      <el-button type="primary" @click="$emit('submit', localGuest)">{{ $t('guest.add') }}</el-button>
+    </el-form-item>
+  </el-form>
+</template>
