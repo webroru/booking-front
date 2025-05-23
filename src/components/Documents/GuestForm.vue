039508994382163651d580e84d9071dfa31ad55c
Igor Nemykin <webroru@gmail.com>
2025-05-23 17:51:09 +0200

    [GREEN-64] WIP additional guest info

--- src/components/Documents/GuestForm.vue
+++ src/components/Documents/GuestForm.vue
@@ -5,6 +5,25 @@
   const { t } = useI18n();
   const props = defineProps({ guest: Object });
   const localGuest = reactive({ ...props.guest });
+  const cityTaxExemptionOptions = [
+    { value: 0, label: t('guest.cityTaxExemptionOptions.0') },
+    { value: 1, label: t('guest.cityTaxExemptionOptions.1') },
+    { value: 2, label: t('guest.cityTaxExemptionOptions.2') },
+    { value: 3, label: t('guest.cityTaxExemptionOptions.3') },
+    { value: 4, label: t('guest.cityTaxExemptionOptions.4') },
+    { value: 5, label: t('guest.cityTaxExemptionOptions.5') },
+    { value: 7, label: t('guest.cityTaxExemptionOptions.7') },
+    { value: 8, label: t('guest.cityTaxExemptionOptions.8') },
+    { value: 10, label: t('guest.cityTaxExemptionOptions.10') },
+    { value: 11, label: t('guest.cityTaxExemptionOptions.11') },
+    { value: 12, label: t('guest.cityTaxExemptionOptions.12') },
+    { value: 14, label: t('guest.cityTaxExemptionOptions.14') },
+    { value: 15, label: t('guest.cityTaxExemptionOptions.15') },
+    { value: 16, label: t('guest.cityTaxExemptionOptions.16') },
+    { value: 17, label: t('guest.cityTaxExemptionOptions.17') },
+    { value: 18, label: t('guest.cityTaxExemptionOptions.18') },
+    { value: 19, label: t('guest.cityTaxExemptionOptions.19') },
+  ];
 </script>
 
 <template>
@@ -40,6 +59,42 @@
     </el-form-item>
     <el-form-item v-if="!guest.documentNumber" :label="t('guest.documentNumber')">
       <el-input v-model="localGuest.documentNumber" />
+    </el-form-item>
+    <el-form-item :label="t('guest.checkOutDate')">
+      <el-col :span="11">
+        <el-date-picker
+            v-model="localGuest.checkoutDate"
+            type="date"
+            :placeholder="t('guest.pickADate')"
+            style="width: 100%"
+        />
+      </el-col>
+      <el-col :span="2" class="text-center">
+        <span class="text-gray-500">-</span>
+      </el-col>
+      <el-col :span="11">
+        <el-time-select
+            v-model="localGuest.checkoutTime"
+            style="width: 100%"
+            start="00:00"
+            step="01:00"
+            end="23:00"
+            :placeholder="t('guest.pickATime')"
+        />
+      </el-col>
+    </el-form-item>
+    <el-form-item :label="t('guest.cityTaxExemption')">
+      <el-select
+          v-model="localGuest.cityTaxExemption"
+          :placeholder="t('guest.select')"
+      >
+        <el-option
+            v-for="item in cityTaxExemptionOptions"
+            :key="item.value"
+            :label="item.label"
+            :value="item.value"
+        />
+      </el-select>
     </el-form-item>
     <el-form-item>
       <el-button type="primary" @click="$emit('submit', localGuest)">{{ $t('guest.add') }}</el-button>
