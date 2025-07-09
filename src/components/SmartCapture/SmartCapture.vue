c7ed65657378aaf0962c37f1c8cf3f5d93ca7e21
Igor Nemykin <igor.nemykin@dhl.com>
2025-07-09 17:07:30 +0200

    [GREEN-63] manual data input

--- src/components/SmartCapture/SmartCapture.vue
+++ src/components/SmartCapture/SmartCapture.vue
@@ -1,7 +1,7 @@
 <script setup>
   import { onMounted, ref, nextTick } from 'vue';
   import { useI18n } from 'vue-i18n';
-  import { defineComponents, DocumentReaderService } from '@regulaforensics/vp-frontend-document-components';
+  import { defineComponents, DocumentReaderService, ErrorTypes } from '@regulaforensics/vp-frontend-document-components';
 
   const { locale } = useI18n({ useScope: 'global' });
   const emit = defineEmits(['recognize', 'error']);
@@ -14,9 +14,23 @@
   };
 
   const listener = (event) => {
+    console.log(event);
     if (event.detail.action !== 'PROCESS_FINISHED') {
       return;
     }
+
+    const errors = [
+      ErrorTypes.WASM_ERROR,
+      ErrorTypes.WASM_LICENSE,
+      ErrorTypes.FILE_SIZE,
+      ErrorTypes.UNKNOWN_ERROR,
+      ErrorTypes.NO_CAMERA,
+      ErrorTypes.INCORRECT_CAMERA_ID,
+      ErrorTypes.CONNECTION_ERROR,
+      ErrorTypes.HTTP_NOT_SUPPORTED,
+      ErrorTypes.INSECURE_PAGE_CONTEXT,
+      ErrorTypes.BAD_CONFIGURATION,
+    ];
 
     if (event.detail.data.status === 1 && event.detail.data.response.text !== undefined) {
       const fieldList = event.detail.data.response.text.fieldList;
@@ -31,6 +45,8 @@
         documentNumber: getValueFromFieldList(fieldList, 'Document Number'),
       };
       emit('recognize', data);
+    } else if (event.detail.data.status === 0 && errors.includes(event.detail.data.reason)) {
+      emit('error', 'fatal');
     } else {
       emit('error');
     }
@@ -41,19 +57,26 @@
     return fieldList.find(item => item.fieldName === fieldName)?.value;
   };
 
-  onMounted(() => {
-    window.RegulaDocumentSDK = new DocumentReaderService();
-    window.RegulaDocumentSDK.recognizerProcessParam = {
-      processParam: {
-        scenario: 'MrzAndLocate',
-      },
-    };
-    window.RegulaDocumentSDK.imageProcessParam = {
-      processParam: {
-        scenario: 'MrzAndLocate',
-      },
-    };
-    defineComponents().then(() => window.RegulaDocumentSDK.initialize({ license: 'AAEAADbnc1dn7ZtGBQ/t6MF+wSnPX2EILZ+QI+UbCMv4UPNpSAcI7aQfXVL9L4QIFPKWKwhqE4TtoP8NDdrz+btmWHkvVHwK3kbOxOYiyvhhhejmzo03XDuO7a0pkdIt7C2K5NDBarRPXyrkZt80noJkWvMe6F0xrPghrpy0HjilbRjK2yxDPORBAhYJQUYTQjN/etD9DhEIGV7NY56lTuifM0FaqyryV9c+mABYlCXNMblN3I+KI2hZNq6KkEcLKlgOtR4PnKTxNp2vBY9WLepUf+DyOO1qg5oVvrrg7jwhhl1eIO/9mXBNmqC9WEKyUbfsLtkTePB/codLq42Ltsq7HHnkAAAAAAAAEMQNhQQDLCnzP7jpFGeLoN4+XnfwFXV8R5I3INybH/s7wMnfZ6FGtD7fgVXv1EnA6mTrco5+b4M/U2Zkl07XIwv81Aef63CVVWdzjF+myhTWmBrnI1WwZZNTYDfdLBRIVTEVpflqG6bWxt0SLbgCelLc6IsL60P65KBLIGTbITPX12FMOEndfxnM/mCUbPAAeFxSX5d13PyKQPTaqVmUCIBKOHQaaMwUjF+0yIGK7hlQqhwBo0hxuwKEN4R4BldpW41QxoteMacI68BN21g/93YyYmLQUdygCyI0pEoQdquY' }));
+  onMounted(async () => {
+    try {
+      window.RegulaDocumentSDK = new DocumentReaderService();
+      window.RegulaDocumentSDK.recognizerProcessParam = {
+        processParam: {
+          scenario: 'MrzAndLocate',
+        },
+      };
+      window.RegulaDocumentSDK.imageProcessParam = {
+        processParam: {
+          scenario: 'MrzAndLocate',
+        },
+      };
+
+      await defineComponents();
+      await window.RegulaDocumentSDK.initialize({ license: 'AAEAADbnc1dn7ZtGBQ/t6MF+wSnPX2EILZ+QI+UbCMv4UPNpSAcI7aQfXVL9L4QIFPKWKwhqE4TtoP8NDdrz+btmWHkvVHwK3kbOxOYiyvhhhejmzo03XDuO7a0pkdIt7C2K5NDBarRPXyrkZt80noJkWvMe6F0xrPghrpy0HjilbRjK2yxDPORBAhYJQUYTQjN/etD9DhEIGV7NY56lTuifM0FaqyryV9c+mABYlCXNMblN3I+KI2hZNq6KkEcLKlgOtR4PnKTxNp2vBY9WLepUf+DyOO1qg5oVvrrg7jwhhl1eIO/9mXBNmqC9WEKyUbfsLtkTePB/codLq42Ltsq7HHnkAAAAAAAAEMQNhQQDLCnzP7jpFGeLoN4+XnfwFXV8R5I3INybH/s7wMnfZ6FGtD7fgVXv1EnA6mTrco5+b4M/U2Zkl07XIwv81Aef63CVVWdzjF+myhTWmBrnI1WwZZNTYDfdLBRIVTEVpflqG6bWxt0SLbgCelLc6IsL60P65KBLIGTbITPX12FMOEndfxnM/mCUbPAAeFxSX5d13PyKQPTaqVmUCIBKOHQaaMwUjF+0yIGK7hlQqhwBo0hxuwKEN4R4BldpW41QxoteMacI68BN21g/93YyYmLQUdygCyI0pEoQdquY' });
+    } catch (error) {
+      console.error('Error initializing RegulaDocumentSDK:', error);
+      emit('error', 'fatal');
+    }
   });
 </script>
 
