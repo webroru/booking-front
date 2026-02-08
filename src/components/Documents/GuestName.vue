3fa61f7e1a91b739efe97e5662bd637bc68c20e3
webroru <webroru@gmail.com>
2026-02-08 11:09:34 +0100

    [GREEN-92] fix popup and removing guests

--- src/components/Documents/GuestName.vue
+++ src/components/Documents/GuestName.vue
@@ -14,6 +14,6 @@
     <el-col :span="2"><el-icon :color="!guest ? 'red' : 'black'"><avatar /></el-icon></el-col>
     <el-col v-if="guest" :span="17">{{guest.firstName }} {{guest.lastName }}</el-col>
     <el-col v-else :span="17"><el-icon :color="'red'"><question-filled /></el-icon><el-icon :color="'red'"><question-filled /></el-icon><el-icon :color="'red'"><question-filled /></el-icon></el-col>
-    <el-col v-if="guest && !guest.id" :span="3"><el-button type="danger" :icon="Delete" circle @click="$emit('remove', index)" /></el-col>
+    <el-col v-if="guest && !guest.id" :span="3"><el-button type="danger" :icon="Delete" circle @click="$emit('remove', index - 1)" /></el-col>
   </el-row>
 </template>
