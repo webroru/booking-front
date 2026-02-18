53eac35c28b1e43db1aab471f389380387ba36a9
Igor Nemykin <igor.nemykin@dhl.com>
2026-02-18 14:23:51 +0100

    [GREEN-109] new navigation store, updated Search and NextButton

--- src/components/Confirmation/NextButton.vue
+++ src/components/Confirmation/NextButton.vue
@@ -1,60 +1,34 @@
 <script setup>
-  import { useBookingStore } from '@/stores/booking';
-  import { useRoute } from 'vue-router';
+import { useRoute } from 'vue-router'
+import { useNavigationStore } from '@/stores/navigation'
 
-  defineProps({
-    disabled: Boolean,
-  });
-
-  const bookingStore = useBookingStore();
-  const route = useRoute();
-  const { booking, bookings } = bookingStore;
-
-  const step = [
-    'search',
-    'instruction',
-    'rules',
-    'documents',
-    'payment',
-    'booking-info',
-  ];
-
-  const currentStep = () => step.findIndex(path => route.path.includes(path));
-  const isLastStep = () => currentStep() >= 5;
-
-  const nextStep = () => {
-    let stepIndex = currentStep();
-    const getDebt = () => Math.max(bookings.reduce((debt, booking) => (debt + booking.debt), 0), 0);
-    if (bookings.every(booking => booking.checkIn) && stepIndex === 0) {
-      return `/confirmation/${booking?.orderId}/booking-info`;
-    }
-    stepIndex++;
-    if (step[stepIndex] === 'payment' && getDebt() === 0) {
-      stepIndex++;
-    }
-    if (stepIndex < 0) {
-      return '/';
-    }
-    return stepIndex === 0 ? '/confirmation/search' : `/confirmation/${booking?.orderId}/${step[stepIndex]}`;
-  };
+const route = useRoute()
+const navigation = useNavigationStore()
 </script>
 
 <template>
-  <router-link v-if="!isLastStep()" :to="nextStep()" :class="{ disabled }">
-    <el-button :disabled="disabled"><b>{{ $t('common.next') }}</b></el-button>
+  <router-link
+      v-if="!navigation.isLastStep(route.path)"
+      :to="navigation.button.to"
+      :class="{ disabled: navigation.button.disabled }"
+  >
+    <el-button :type="navigation.button.variant" :disabled="navigation.button.disabled">
+      <b>{{ navigation.button.label }}</b>
+    </el-button>
   </router-link>
-  <router-link v-if="isLastStep()" to="/">
+
+  <router-link v-else to="/">
     <el-button>{{ $t('common.exit') }}</el-button>
   </router-link>
 </template>
 
 <style scoped>
-  a {
-    margin-top: 12px;
-    text-decoration: none;
-  }
+a {
+  margin-top: 12px;
+  text-decoration: none;
+}
 
-  .disabled {
-    pointer-events: none;
-  }
+.disabled {
+  pointer-events: none;
+}
 </style>
