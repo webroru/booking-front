3dda2d59ba0a66e813f332065c64088f284f359b
Igor Nemykin <igor.nemykin@dhl.com>
2026-02-19 15:33:26 +0100

    [GREEN-109] update nav

--- src/stores/navigation.ts
+++ src/stores/navigation.ts
@@ -1,10 +1,8 @@
 import { defineStore } from 'pinia'
 import { reactive } from 'vue'
 import { useI18n } from 'vue-i18n';
-import { useBookingStore } from '@/stores/booking'
 
 export const useNavigationStore = defineStore('navigation', () => {
-    const bookingStore = useBookingStore()
     const { t } = useI18n();
 
     const button = reactive({
@@ -15,62 +13,7 @@ export const useNavigationStore = defineStore('navigation', () => {
         to: '/',
     })
 
-    const steps = [
-        'search',
-        'instruction',
-        'rules',
-        'documents',
-        'payment',
-        'booking-info',
-    ]
-
-    const getCurrentStepIndex = (path: string) =>
-        steps.findIndex(step => path.includes(step))
-
-    const isLastStep = (path: string) =>
-        getCurrentStepIndex(path) >= steps.length - 1
-
-    const getDebt = () =>
-        Math.max(bookingStore.bookings.reduce((d, b) => d + (b.debt ?? 0), 0), 0)
-
-    const getNextRoute = (path: string): string => {
-        const { booking, bookings } = bookingStore
-        let stepIndex = getCurrentStepIndex(path)
-
-        if (bookings.every(b => b.checkIn) && stepIndex === 0) {
-            return `/confirmation/${booking?.orderId}/booking-info`
-        }
-
-        stepIndex++
-
-        if (steps[stepIndex] === 'payment' && getDebt() === 0) {
-            stepIndex++
-        }
-
-        if (stepIndex < 0) return '/'
-
-        return stepIndex === 0
-            ? '/confirmation/search'
-            : `/confirmation/${booking?.orderId}/${steps[stepIndex]}`
-    }
-
-    const setNextButton = (options: Partial<typeof button>) => {
-        Object.assign(button, options)
-    }
-
-    const resetButton = () => {
-        button.label = 'Next'
-        button.disabled = false
-        button.variant = 'primary'
-        button.to = '/'
-    }
-
     return {
         button,
-        steps,
-        isLastStep,
-        getNextRoute,
-        setNextButton,
-        resetButton,
     }
 })
